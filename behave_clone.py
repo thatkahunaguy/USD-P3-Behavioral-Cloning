2587c4cd1415879f93cc01ec4c56200cb181a8d5
John Glancy <jeglancy@gmail.com>
2017-04-28 10:24:17 -0700

    fix: corrected generator to only generate batch_samples # of samples with a random      mix of samples augmented by camera and flip within this batch fix: modify steering angle adjustmend from .25 to .15 as .25 seems to overcorrect      based on inspection of images reviewed in Behavioral_Clone_test.ipynb

--- behave_clone.py
+++ behave_clone.py
@@ -2,15 +2,15 @@ import os
 import csv
 import cv2
 import numpy as np
+import random
 from sklearn.model_selection import train_test_split
-from random import shuffle
 from keras.models import Sequential
 from keras.layers import Cropping2D, Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense, Lambda, Dropout
 
 # set a path and data file so we can move between small test files
 # and full training data sets easily
-path = './data/'
+path = './training_data/'
 # driving_log.csv or test_log.csv
 data_file = 'driving_log.csv'
 
@@ -26,13 +26,14 @@ with open(file) as csvfile:
 
 # The sample data includes a line of column headers in the file, delete this
 # TODO remove if collecting your own data as the headers dont get generated
-del samples[0]
+#del samples[0]
 
 # TODO: only for debug, show image shape
 file = path + 'IMG/' + samples[1][0].split('/')[-1]
 print("test file is: ", file)
 image = cv2.imread(file)
 print("Image shape is: ", image.shape)
+print("total samples: ", len(samples))
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
@@ -40,31 +41,46 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-        # we shuffle samples in the generator.  Without a generator we
+        # shuffle samples in the generator.  Without a generator we
         # can simply shuffle with on option in model.fit
-        shuffle(samples)
+        random.shuffle(samples)
+        # TODO think about whether shuffling in generator makes sense?
+        # does this make it possible to never get a reasonable number of images
+        # from small representations of the track like the dirt patches the
+        # trained model seems to be having problems with?
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             angles = []
+            # define our cameras
+            cameras = ['center', 'left', 'right']
+            # camera angle adjustment center, left, right
+            angle_adjustment = [0., 0.15, -0.15]
+            # note that I had to refactor the generator to only return
+            # batch_size images as I was originally augmenting all images
+            # in the batch resulting in 6xbatch_size images see this post
+            # https://discussions.udacity.com/t/implementing-generator-help/230608/4
             for batch_sample in batch_samples:
-                # add 3 camera images adjusted for camera angle as if centered
-                # camera angle adjustment center, left, right
-                angle_adjustment = [0., 0.25, -0.25]
-                for i in range(0,3):
+                # for i in range(0,3): originally used all cameras
+                # choose a random camera - this assumes we'll get a good mix
+                # from random sampling
+                camera = random.randint(0, len(cameras)-1)
                 # adjust the filename/path since orig logged on a local machine
                 # filename is the last part of the sample path
-                    file = path + 'IMG/' + batch_sample[i].split('/')[-1]
-                    image = cv2.imread(file)
-                    angle = float(batch_sample[3]) + angle_adjustment[i]
-                    images.append(image)
-                    angles.append(angle)
-                    # augment with a flipped version of this image since track
-                    # is primarily left turning
-                    image = image[:, ::-1, :]
-                    images.append(image)
-                    angles.append(-angle)
+                file = path + 'IMG/' + batch_sample[camera].split('/')[-1]
+                image = cv2.imread(file)
+                angle = float(batch_sample[3]) + angle_adjustment[camera]
+                images.append(image)
+                angles.append(angle)
+
+            # augment by flipping half the images since track
+            # is primarily left turning (originally appended all flipped)
+            print("# of images: ", len(images))
+            indices = random.sample(range(len(images)), int(len(images)/2))
+            print("indices: ",indices)
+            for i in indices:
+                images[i] = images[i][:, ::-1, :]
+                angles[i] = -angles[i]
 
             # convert image and angle data to np arrays as reqd by Keras
             X_train = np.array(images)
